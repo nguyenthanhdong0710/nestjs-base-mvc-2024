3812d77cc2b09fcfbe6eee53f042b884328302ab
nguyenthanhdong0710 <nguyenthanhdong0710@gmail.com>
2024-02-21 03:42:26 +0700

    add express-handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
 import { join } from 'path';
+import { create } from 'express-handlebars';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  const hbs = create({ extname: 'hbs' });
+  app.engine('hbs', hbs.engine);
   app.setViewEngine('hbs');
 
   await app.listen(3000);
